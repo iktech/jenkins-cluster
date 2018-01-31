572d7f69f5d23c385979cac7d88b5ae4a0f22b2a
Igor Kolomiyets <ikolomiyets@gmail.com>
2018-01-31 12:42:38 +0000

    Changing Jenkins swarm execution parameters

--- jenkins-slave/jenkins-slave.sh
+++ jenkins-slave/jenkins-slave.sh
@@ -32,9 +32,9 @@ elif [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
   # jenkins swarm slave
   JAR=`ls -1 /opt/jenkins-slave/bin/swarm-client-*.jar | tail -n 1`
 
-  if [[ "$@" != *"-master "* ]] && [ ! -z "$JENKINS_PORT_8080_TCP_ADDR" ]; then
-	PARAMS="-master http://${JENKINS_SERVICE_HOST}:${JENKINS_SERVICE_PORT}${JENKINS_CONTEXT_PATH} -tunnel ${JENKINS_SLAVE_SERVICE_HOST}:${JENKINS_SLAVE_SERVICE_PORT}${JENKINS_SLAVE_CONTEXT_PATH} -username ${master_username} -password ${master_password} -executors ${slave_executors}"
-  fi
+#  if [[ "$@" != *"-master "* ]] && [ ! -z "$JENKINS_PORT_8080_TCP_ADDR" ]; then
+	PARAMS="-master https://${JENKINS_SERVICE_HOST}${JENKINS_CONTEXT_PATH} -tunnel ${JENKINS_SLAVE_SERVICE_HOST}:${JENKINS_SLAVE_SERVICE_PORT}${JENKINS_SLAVE_CONTEXT_PATH} -username ${master_username} -password ${master_password} -executors ${slave_executors}"
+#  fi
 
   echo Running java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
   exec java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
